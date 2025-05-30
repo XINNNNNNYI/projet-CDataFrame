5bd5727aa79543858ca3d6616e819d6b7b89077c
TOZZZ-to <lalamzian123@gmail.com>
2025-05-30 12:29:23 +0200

    Create dataframe.h

---
+++ dataframe.h
@@ -0,0 +1,19 @@
+#ifndef PROJET_C_DATAFRAME_H
+#define PROJET_C_DATAFRAME_H
+#include "colonne.h"
+typedef struct{
+    COLUMN**colonne;
+    int nombre_colonne;
+    int nombre_ligne;
+}dataframe;
+dataframe creer_cdataframe(dataframe*df);
+int ajouter_colonne(dataframe*df);
+int ajouter_ligne(dataframe*df);
+int remplir_datafarme(dataframe*df);
+
+dataframe afficher_cdataframe(dataframe*df);
+
+
+
+
+#endif
