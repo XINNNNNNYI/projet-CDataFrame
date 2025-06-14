630f85e63fca46604c7e6671f867b3c912a53ec0
TOZZZ-to <lalamzian123@gmail.com>
2025-06-15 01:08:27 +0200

    Update new_colonne.h

--- new_colonne.h
+++ new_colonne.h
@@ -1,20 +1,36 @@
+//
+// Created by MOUMSIK on 14/06/2025.
+//
+
 #ifndef PROJET_C_NEW_COLONNE_H
 #define PROJET_C_NEW_COLONNE_H
 
 #include <stdio.h>
+#include <stdlib.h>   // Pour malloc/free
+#include <string.h>   // Pour strdup
+
 
 #define REALLOC_SIZE 256
 
 typedef enum {
     NULLVAL = 1,
-    UNINT,
+    UINT,
     INT,
     CHAR,
     FLOAT,
     DOUBLE,
     STRING,
-    OBJECT
+    USHORT,
+    OBJECT,
+    SHORT,
+    ULONG,
+    LONG,
+    UCHAR,
+   
+
+
 } Enum_Type;
+
 
 typedef union {
     unsigned int uint_value;
@@ -30,14 +46,28 @@ typedef union {
     char* string_value;
     void* object_value;
 } Column_Type;
+typedef enum{
+    OBJECT_UNINT,
+    OBJECT_INT,
+    OBJECT_CHAR,
+    OBJECT_FLOAT,
+    OBJECT_DOUBLE,
+    OBJECT_STRING,
 
-typedef struct Column {
-    char* column_name;
+}Enum_Object;
+typedef struct{
+    Enum_Object type;
+    Column_Type value;
+
+
+}Objet_value;
+
+typedef struct  {
+    char* titre;
     unsigned int taille_logique;
     unsigned int taille_physique;
     Enum_Type column_type;
-    Column_Type** donnee;  
-
+    Column_Type** donnee;
     unsigned long long int* index;
     char valid_index;
     char sort_dir;
@@ -45,15 +75,12 @@ typedef struct Column {
 } Column;
 
 // Fonctions associées
-Column* create_column(Enum_Type type, char* column_name);
+Column* create_column(Enum_Type type, char* titre);
 int insert_value(Column* col, void* value);
 void delete_column(Column* col);
-void print_value(Column* col, unsigned long long int i, char* str, int size);
+void print_object(Objet_value*ob, char* str, unsigned int taille_logique );
+void print_value(Column* col, unsigned long long int i, char* str, unsigned int taille_logique);
 void print_col(Column* col);
 void info_column(Column* col);
 
 #endif // PROJET_C_NEW_COLONNE_H
-
-
-
-
