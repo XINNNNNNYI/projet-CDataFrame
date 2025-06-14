c527146bad61730731216cc6251e2b82b4985bb2
TOZZZ-to <lalamzian123@gmail.com>
2025-06-14 14:49:11 +0200

    Update colonne.h

--- colonne.h
+++ colonne.h
@@ -1,22 +1,24 @@
 #ifndef PROJET_C_COLONNE_H
 #define PROJET_C_COLONNE_H
-#define REALLOC_Size 256
-typedef struct{
-    char*titre;
-    int donnée;
+
+#include <stdio.h>
+
+#define REALLOC_SIZE 256
+
+typedef struct {
+    char* titre;
+    int* donnees;
     int taille_physique;
     int taille_logique;
-}COLUMM;
-COLUMN*create_column(char*titre);
-int insert_value(COLUMN$col, int value);
-void delete_column(COLUMN*col);
-void print_col(COLUMN*col);
-int nombre_occureneces(COLLUMN*col, int valeure);
-int indice_position(COLUMN*col, int valeure);
-int valeur_supereur(COLUMN*col, int valeure);
-int valeur_inferieur(COLUMN*col, int valeure);
+} Column;
 
+Column* create_column(char* titre);
+void insert_value(Column* col, int value);
+void delete_column(Column* col);
+void print_col(Column* col);
+int nombre_occurences(Column* col, int valeur);
+int indice_position(Column* col, int valeur);
+int valeur_superieure(Column* col, int valeur);
+int valeur_inferieure(Column* col, int valeur);
 
-
-
-#endif //PROJET_C_COLONNE_H
+#endif
