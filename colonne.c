d6446e10a25cf3ec3005006e7683009c77817b8d
TOZZZ-to <lalamzian123@gmail.com>
2025-06-14 14:50:01 +0200

    Update colonne.c

--- colonne.c
+++ colonne.c
@@ -1,89 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "colonne.h"
-COLUMN*create_column(char*titre){
-    COLUMN*col=malloc(sizeof(COLUMN));
-    if(col==NULL) return NULL;
-    col->titre=titre;
-    col->donnee=NULL;
-    col->taille_physique=0;
-    col->taille_logique=0;
+
+Column* create_column(char* titre) {
+    Column* col = malloc(sizeof(Column));
+    if (col == NULL) return NULL;
+    col->titre = titre;
+    col->donnees = NULL;
+    col->taille_physique = 0;
+    col->taille_logique = 0;
     return col;
-
-
 }
-// fonction ajoutant une valeur à la colonne
-int insert_value(COLUMN*col, int value){
-    if(col==NULL)
+
+// Fonction ajoutant une valeur à la colonne
+int insert_value(Column* col, int value) {
+    if (col == NULL)
         return 0;
-    if(col->taile_physique==0) {
-        col->donnee = malloc(REALLOC_Size * sizeof(int));
-        if (col->donnee == NULL)
+
+    if (col->taille_physique == 0) {
+        col->donnees = malloc(REALLOC_SIZE * sizeof(int));
+        if (col->donnees == NULL)
             return 0;
         col->taille_physique = REALLOC_SIZE;
     }
-    else if(col->taille_physique >= col->taille_logique ){
-        int *data=realloc(col->donnee,(col->taille_physqiue+REALLOC_Size));
-        if (dat==NULL)
+    else if (col->taille_logique >= col->taille_physique) {
+        int* data = realloc(col->donnees, (col->taille_physique + REALLOC_SIZE) * sizeof(int));
+        if (data == NULL)
             return 0;
-        col->taille_physique+=REALLOC_Size
-        col->donnee=data;}
-    col->donnee[col->taille_logique++]  = value;
+        col->donnees = data;
+        col->taille_physique += REALLOC_SIZE;
+    }
 
+    col->donnees[col->taille_logique++] = value;
+    return 1;
+}
 
-    return 1:}
-void delete_column(COLUMN*col) {
+void delete_column(Column* col) {
     if (col != NULL) {
         free(col->titre);
-        free(col->donnee);
+        free(col->donnees);
         free(col);
     }
 }
-void print_col(COLUMN*col){
 
-    if(col!=NULL) {
-        for (int i = 0; i < (col->taille_logique), i++) {
-            printf("[%d] %d", i, col->donnee[i])
-
-
+void print_col(Column* col) {
+    if (col != NULL) {
+        for (int i = 0; i < col->taille_logique; i++) {
+            printf("[%d] %d\n", i, col->donnees[i]);
         }
     }
 }
-int nombre_occureneces(COLLUMN*col, int valeure){
-    int oc=0;
-    for(int i=0; i<col->taille_loique){
-        if(c->donnee[i]==valeure){
+
+int nombre_occurences(Column* col, int valeur) {
+    int oc = 0;
+    for (int i = 0; i < col->taille_logique; i++) {
+        if (col->donnees[i] == valeur) {
             oc++;
         }
-
     }
     return oc;
 }
-int indice_position(COLUMN*col, int pos){
-    if(pos>=col->taille_logique)
+
+int indice_position(Column* col, int pos) {
+    if (pos >= col->taille_logique)
         return 0;
-   return col->donnee[pos]);
+    return col->donnees[pos];
 }
-int valeur_supereur(COLLUMN*col, int valeure) {
+
+int valeur_superieure(Column* col, int valeur) {
     int oc = 0;
-    for (int i = 0; i < col->taille_loique) {
-        if (c->donnee[i] > valeure) {
+    for (int i = 0; i < col->taille_logique; i++) {
+        if (col->donnees[i] > valeur) {
             oc++;
         }
-
     }
     return oc;
 }
 
-int valeur_inferieur(COLLUMN*col, int valeure) {
+int valeur_inferieure(Column* col, int valeur) {
     int oc = 0;
-    for (int i = 0; i < col->taille_loique) {
-        if (c->donnee[i] < valeure) {
+    for (int i = 0; i < col->taille_logique; i++) {
+        if (col->donnees[i] < valeur) {
             oc++;
         }
-
     }
     return oc;
 }
-
-
